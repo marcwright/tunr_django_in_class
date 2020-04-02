89b6b2ab9463d899cbd3afe1d614ca6f4e8e53e6
Marc Wright <marc@generalassemb.ly>
2020-04-02 14:03:37 -0500

    Next: base.html and CSS

--- tunr/views.py
+++ tunr/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Artist, Song
+
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'tunr/artist_list.html', {'artists': artists})
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'tunr/song_list.html', {'songs': songs})
+
+
+def artist_detail(request, pk):
+    artist = Artist.objects.get(id=pk)
+    return render(request, 'tunr/artist_detail.html', {'artist': artist})
+
+
+def song_detail(request, pk):
+    song = Song.objects.get(id=pk)
+    return render(request, 'tunr/song_detail.html', {'song': song})
