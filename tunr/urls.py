16f74ce11d98ba028be3a5cb1e17400aaed17adb
Marc Wright <marc@generalassemb.ly>
2020-04-02 14:36:47 -0500

    Finish song create

--- tunr/urls.py
+++ tunr/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.artist_list, name='artist_list'),
     path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
+    path('artists/new', views.artist_create, name='artist_create'),
     path('songs/', views.song_list, name='song_list'),
-    path('songs/<int:pk>', views.song_detail, name='song_detail')
+    path('songs/<int:pk>', views.song_detail, name='song_detail'),
+    path('songs/new', views.song_create, name='song_create')
 ]
--- tunr/views.py
+++ tunr/views.py
@@ -1,23 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Artist, Song
+from .forms import ArtistForm, SongForm
 
 
 def artist_list(request):
     artists = Artist.objects.all()
     return render(request, 'tunr/artist_list.html', {'artists': artists})
 
+def artist_detail(request, pk):
+    artist = Artist.objects.get(id=pk)
+    return render(request, 'tunr/artist_detail.html', {'artist': artist})
+
+def artist_create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            artist = form.save()
+            return redirect('artist_detail', pk=artist.pk)
+    else:
+        form = ArtistForm()
+    return render(request, 'tunr/artist_form.html', {'form': form})
 
 def song_list(request):
     songs = Song.objects.all()
     return render(request, 'tunr/song_list.html', {'songs': songs})
 
-
-def artist_detail(request, pk):
-    artist = Artist.objects.get(id=pk)
-    return render(request, 'tunr/artist_detail.html', {'artist': artist})
-
-
 def song_detail(request, pk):
     song = Song.objects.get(id=pk)
     return render(request, 'tunr/song_detail.html', {'song': song})
+
+def song_create(request):
+    if request.method == 'POST':
+        form = SongForm(request.POST)
+        if form.is_valid():
+            song = form.save()
+            return redirect('song_detail', pk=song.pk)
+    else:
+        form = SongForm()
+    return render(request, 'tunr/song_form.html', {'form': form})
